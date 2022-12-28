44c4c7ed748f2f2252326d1112f9e0bbe53ae63b
zhus-k <76853151+zhus-k@users.noreply.github.com>
2022-12-28 16:20:05 -0500

    dynamically create axios client by app env

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -1,12 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default ({ req }) => {
-	if (typeof window === "undefined") {
-		return axios.create({
-			baseURL: "http://www.ppaxit.click/",
-			headers: req.headers,
-		});
-	} else {
-		return axios.create();
-	}
+  if (typeof window === 'undefined') {
+    return axios.create({
+      baseURL:
+        process.env.NODE_ENV === 'production'
+          ? 'http://www.ppaxit.click/'
+          : 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      headers: req.headers,
+    });
+  } else {
+    return axios.create();
+  }
 };
